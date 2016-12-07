32a1543b959eeed6cbf6bf264210ededeea72ba4
Marcode777 <marcosebello7@gmail.com>
2016-12-07 13:24:58 -0500

    created src folder with index.js file in it, created components folder within src folder, with app.js file in it

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-alert("yeah");
-alert("it works");
+console.log("yeah, this works"); //this shows that index.js is working
+let message = 'HELLO FROM ENTRY!!!'; // this shows that both index.js and es6 is working as well (also, just changing any part of these messages and then after saving, if the changes update, then that's confirmation that live-reloading is working as well)
+console.log(message); // this shows that both index.js and es6 is working as well also
+
+import React from 'react';
+import {render} from 'react-dom';
+import App from 'components/app';
+
+render(<App/>, document.getElementById('app')); // this is the id where the react app will actually be displayed into the html page
