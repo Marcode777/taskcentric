6a3d096bd3e82a2870187df741db36e38fb4daf5
Marcode777 <marcosebello7@gmail.com>
2016-12-07 15:37:40 -0500

    installed lodash, added src/components/tasklistitem.js

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Tasklist from './tasklist';
 
 
-const todos = [    // todos will be changed to tasks
+const tasks = [    // todos will be changed to tasks
   {
     task:" do taskcentric",
     isCompleted: false
@@ -18,10 +18,16 @@ const todos = [    // todos will be changed to tasks
 // now we want to be able to take the todos (which will be changed to tasks) array into the tasklist, however, we also want to be able to access that data from our tasklist, and the way to do that is to put it into our tasklist component, but we want to be able to re-render whenever the props change, so that we want to be make our todos (task) array the state of our app
 // we'll have a constructor(props) which is the first thing this component will run and then a super(props), which sort of just connects it to the parent that it's inheriting from, this is generally standard syntax
 // React uses state, which you can use set.state with to update the state and re-render components, in this case, we'll use this.state
+// now to pass our state of tasks into tasklist, we'll pass the constant tasks into it, we do this by doing this.state={tasks]; and then tasks={this.state.tasks}
+// now our Tasklist component has our tasklist that we're passing in
  export default class App extends React.Component {
 
   constructor(props){
     super(props);
+
+    this.state={
+      tasks
+    };
   }
 
 
@@ -29,7 +35,7 @@ const todos = [    // todos will be changed to tasks
     return(
       <div>
         <h1>taskcentric</h1>
-        <Tasklist/> 
+        <Tasklist tasks={this.state.tasks} /> 
       </div>
       );
   }
