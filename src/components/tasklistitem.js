4d53b36dd235e47b9f3e4ebedce4b45d6d9dc4da
Marcode777 <marcosebello7@gmail.com>
2016-12-08 14:46:08 -0500

    figured out why onClicking the items did not change color, tasks was to be used in the first tasks, as in tasks => tasks.task===task

--- src/components/tasklistitem.js
+++ src/components/tasklistitem.js
@@ -4,7 +4,8 @@ import React from 'react';
 // onCLick, and the like, are how we do event handlers in React
 // with the renderActionsSection, if the state is editing, we return the Save button and also the Cancel button
 // with the renderActionsSection, if the state is not editing, and here, we actually don't need an else statement, because it just returns , we return the Edit button and also the Delete Button
-// for dynamic styling, we create a const component called taskStyle, which will be a conditional, so if, it isCompleted, it will be one color, otherwise it will be the other  
+// for dynamic styling, we create a const component called taskStyle, which will be a conditional, so if, it isCompleted, it will be one color, otherwise it will be the other 
+// now to have the items change in color when they are clicked, to show whether they are completed or not, we make an onClick handler and insert it into the return task section, along with the taskStyle component that's inserted within it as well, but we want to actually modify the actual array, so it will happen in the app.js file, where we will define toggleTask() method
 export default class TaskListItem extends React.Component {
 
   constructor(props){
@@ -17,6 +18,7 @@ export default class TaskListItem extends React.Component {
 
   renderTasksSection(){
     const {task, isCompleted} = this.props;
+    console.log(this.props);
 
     const taskStyle = {
       color: isCompleted ? '#809fff' : '#ff9999',
@@ -24,7 +26,7 @@ export default class TaskListItem extends React.Component {
     }
 
     return(
-      <td style={taskStyle} >{task}</td>
+      <td style={taskStyle} onClick={this.props.toggleTask.bind(this, task)} >{task}</td>
       )
   }
 
